d01826d1287decce41f6e296733d22be8d6968bd
MBP <dev@tbk81.com>
2026-05-04 14:23:16 -0700

    create df from list

--- Intro_to_pandas/01-Create-DF-from-list/df_to_list.py
+++ Intro_to_pandas/01-Create-DF-from-list/df_to_list.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
+data = [
+  [1, 15],
+  [2, 11],
+  [3, 11],
+  [4, 20]
+]
+
+df = pd.DataFrame(data, columns=["student ID", "age"])
+
 def create_dataframe(student_data: List[List[int]]) -> pd.DataFrame:
     pass
 
