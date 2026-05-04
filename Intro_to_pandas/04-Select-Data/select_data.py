96ba33125312688c4cd45f0bc4ff6fac1973b1c9
MBP <dev@tbk81.com>
2026-05-04 16:18:47 -0700

    select data

---
+++ Intro_to_pandas/04-Select-Data/select_data.py
@@ -0,0 +1,8 @@
+"""
+Write a solution to select the name and age of the student with student_id = 101.
+"""
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    pass
