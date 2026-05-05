215f5bfdef59bc53cf3eccb0e01c9f9cfceba634
MBP <dev@tbk81.com>
2026-05-04 17:02:11 -0700

    select data

--- Intro_to_pandas/04-Select-Data/select_data.py
+++ Intro_to_pandas/04-Select-Data/select_data.py
@@ -4,5 +4,5 @@ Write a solution to select the name and age of the student with student_id = 101
 
 import pandas as pd
 
-def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    pass
+def select_data(students: pd.DataFrame) -> pd.DataFrame:
+    return students.loc[students['student_id'] == 101, ['name', 'age']]
